b644e78ca0a6a22be20ff34f127f2f3624f18664
calia07 <71044190+calia07@users.noreply.github.com>
2022-04-02 13:57:19 +0900

    Create App.js

---
+++ Javascript/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {SafeAreaView, Button} from 'react-native';
+import Greeting from './components/Greeting';
+import Box from './components/Box';
+
+const App = () => {
+  const [visible, setVisible] = useState(true);
+  const onPress = () => {
+    setVisible(!visible);
+  };
+  return (
+    <SafeAreaView>
+      <Button title="토글" onPress={onPress} />
+      {visible ? <Box rounded={true} size="large" color="blue" /> : null}
+    </SafeAreaView>
+  );
+};
+export default App;
